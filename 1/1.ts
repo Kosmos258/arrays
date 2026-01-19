0f57788e1e8fc64f89c4901e159b533f45649560
Kosmos258 <maksimowkirill@gmail.com>
2026-01-19 17:22:42 +0300

    add func 'for'

--- 1/1.ts
+++ 1/1.ts
@@ -1,3 +1,9 @@
 function invert(arr: number[]) {
     return arr.map(num => -num);
 }
+
+function invertFor(arr: number[]) {
+    for (let i = 0; i < arr.length; i++) {
+        return -arr[i];
+    }
+}
